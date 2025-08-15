9a39343edc035f3a96646f24ddebdaad694ba68c
Tyler from Zeromatter <tyler@0m.dev>
2025-08-15 11:20:32 -0700

    Use prettyplease instead

--- tool/src/rust/mod.rs
+++ tool/src/rust/mod.rs
@@ -18,7 +18,6 @@ struct DiplomatBridgeFiles<'tcx, 'ccx> {
     entry: &'tcx Path,
     files: &'tcx FileMap,
     errors: &'tcx ErrorStore<'ccx, String>,
-    rust_fmt: PathBuf,
 }
 
 // VisitMut to strip out unwanted files when ultimately outputting to our binding directory.
@@ -46,42 +45,6 @@ impl<'tcx, 'ccx> VisitMut for DiplomatBridgeFiles<'tcx, 'ccx> {
                 let mut file = file.unwrap();
                 self.visit_file_mut(&mut file);
 
-                // TODO: Will this work everywhere?
-                let run = Command::new(self.rust_fmt.clone())
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::piped())
-                    .spawn();
-                if let Err(e) = &run {
-                    self.errors
-                        .push_error(format!("Could not run rustfmt: {e}"));
-                    return;
-                }
-                let mut run = run.unwrap();
-                let stdin = run.stdin.take();
-
-                if let None = stdin {
-                    self.errors
-                        .push_error(format!("Could not get stdin for process: {run:?}"));
-                    return;
-                }
-
-                let err = stdin
-                    .unwrap()
-                    .write_all(file.to_token_stream().to_string().as_bytes());
-                if let Err(e) = err {
-                    self.errors
-                        .push_error(format!("Could not write file for formatting: {e}"));
-                    return;
-                }
-
-                let out = run.wait_with_output();
-
-                if let Err(e) = out {
-                    self.errors
-                        .push_error(format!("Could not get stdout for process: {e}"));
-                    return;
-                }
-
                 self.files.add_file(
                     try_mod_path
                         .file_name()
@@ -89,7 +52,7 @@ impl<'tcx, 'ccx> VisitMut for DiplomatBridgeFiles<'tcx, 'ccx> {
                         .to_str()
                         .unwrap()
                         .to_string(),
-                    String::from_utf8(out.unwrap().stdout).unwrap(),
+                    prettyplease::unparse(&file),
                 );
             } else {
                 self.errors
@@ -111,28 +74,11 @@ pub(crate) fn run<'tcx>(entry: &Path) -> (FileMap, ErrorStore<'tcx, String>) {
     let src = std::fs::read_to_string(entry).unwrap();
     let mut res = syn::parse_file(&src).unwrap();
 
-    // Like cargo expand, we just call rustfmt:
-    let rust_fmt = match env::var_os("RUSTFMT") {
-        Some(which) if !which.is_empty() => Some(PathBuf::from(which)),
-        None => toolchain_find::find_installed_component("rustfmt"),
-        _ => None,
-    };
-
-    let rust_fmt = if let Some(rust_fmt) = rust_fmt {
-        rust_fmt
-    } else {
-        errors.push_error(
-            "Could not find rustfmt command, needed for pretty-printing files.".to_string(),
-        );
-        return (files, errors);
-    };
-
     let mut main = DiplomatBridgeFiles {
         // TODO: Better entry parsing.
         entry: entry.parent().unwrap_or(Path::new("./")),
         files: &files,
         errors: &errors,
-        rust_fmt,
     };
     main.visit_file_mut(&mut res);
 
