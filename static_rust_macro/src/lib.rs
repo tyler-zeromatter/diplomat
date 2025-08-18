55d920653c55c08e772593cc30e6fc0db8f11d1f
Tyler from Zeromatter <tyler@0m.dev>
2025-08-18 11:41:39 -0700

    Basic macro implementation

--- static_rust_macro/src/lib.rs
+++ static_rust_macro/src/lib.rs
@@ -1,4 +1,26 @@
+use diplomat_core::ast;
+use syn::{parse2, parse_macro_input, ItemMod};
+use quote::{quote, ToTokens};
+
+fn gen_bridge(mut input : ItemMod) -> ItemMod {
+    let module = ast::Module::from_syn(&input, true);
+    
+    let (brace, mut new_contents) = input.content.unwrap();
+
+    new_contents.push(parse2(quote! { use diplomat_runtime::*; }).unwrap());
+
+    ItemMod {
+        attrs: input.attrs,
+        vis: input.vis,
+        mod_token: input.mod_token,
+        ident: input.ident,
+        content: Some((brace, new_contents)),
+        semi: input.semi,
+        unsafety: None,
+    }
+}
+
 #[proc_macro_attribute]
 pub fn bridge(_attr: proc_macro::TokenStream, input : proc_macro::TokenStream) -> proc_macro::TokenStream {
-    input
+    gen_bridge(parse_macro_input!(input)).to_token_stream().into()
 }
