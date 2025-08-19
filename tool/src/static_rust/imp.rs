4bafd86a7b5a27a7109b4249749b8f1a8929a761
Tyler from Zeromatter <tyler@0m.dev>
2025-08-19 12:05:16 -0700

    Draft function calls

--- tool/src/static_rust/imp.rs
+++ tool/src/static_rust/imp.rs
@@ -3,7 +3,9 @@ use std::borrow::Cow;
 use askama::Template;
 use diplomat_core::hir::Method;
 
-struct FunctionInfo<'tcx> {
+#[derive(Template)]
+#[template(path = "static_rust/function.rs.jinja", escape = "none")]
+pub(super) struct FunctionInfo<'tcx> {
     name : Cow<'tcx, str>,
     abi_name : Cow<'tcx, str>,
 }
@@ -12,20 +14,12 @@ impl<'tcx> FunctionInfo<'tcx> {
     fn gen_function_info(method : &'tcx Method) -> Self {
         FunctionInfo { name: method.name.as_str().into(), abi_name: method.abi_name.as_str().into() }
     }
-}
 
-#[derive(Template)]
-#[template(path = "static_rust/function_block.rs.jinja", escape = "none")]
-pub(super) struct FunctionBlock<'tcx> {
-    functions : Vec<FunctionInfo<'tcx>>,
-}
-
-impl<'tcx> FunctionBlock<'tcx> {
-    pub(super) fn gen_function_block(functions : impl Iterator<Item = &'tcx Method>) -> FunctionBlock<'tcx> {
+    pub(super) fn gen_function_block(functions : impl Iterator<Item = &'tcx Method>) -> Vec<FunctionInfo<'tcx>> {
         let mut funcs = Vec::new();
         for f in functions {
             funcs.push(FunctionInfo::gen_function_info(f));
         }
-        FunctionBlock { functions: funcs, }
+        funcs
     }
 }
