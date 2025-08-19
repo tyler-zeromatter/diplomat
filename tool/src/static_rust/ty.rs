5c63b5d0125c9064cea4a275d528e3865dc9bb34
Tyler from Zeromatter <tyler@0m.dev>
2025-08-19 11:10:30 -0700

    Fleshing out more of rust generation

--- tool/src/static_rust/ty.rs
+++ tool/src/static_rust/ty.rs
@@ -1,33 +1,42 @@
+use std::borrow::Cow;
+
 use askama::Template;
-use diplomat_core::hir::{StructDef, TypeDef};
+use diplomat_core::hir::{StructDef, TypeDef, TypeId};
 
-pub(super) struct TyGenContext {
+use crate::static_rust::RustFormatter;
 
+pub(super) struct TyGenContext<'tcx> {
+    formatter : &'tcx RustFormatter<'tcx>,
+    id: TypeId,
 }
 
 pub(super) trait TypeTemplate<'a> : Template {}
 
-impl TyGenContext {
-    pub(super) fn from_type(ty : TypeDef) -> impl TypeTemplate {
+impl<'tcx, 'rcx> TyGenContext<'tcx> {
+    pub(super) fn from_type<'a>(id : TypeId, ty : TypeDef<'a>, formatter : &'a RustFormatter) -> impl TypeTemplate<'a> {
+        let ctx = TyGenContext {
+            formatter,
+            id,
+        };
         match ty {
             TypeDef::Struct(st) => {
-                Self::generate_struct(st)
+                ctx.generate_struct(st)
             }
             _ => panic!("Unsupported HIR type {ty:?}")
         }
     }
 
-    fn generate_struct(ty : &StructDef) -> impl TypeTemplate<'_> {
+    fn generate_struct(&'rcx self, ty : &'tcx StructDef) -> impl TypeTemplate<'tcx> {
         #[derive(Template)]
         #[template(path = "static_rust/base.rs.jinja", escape = "none")]
         struct StructTemplate<'a> {
-            struct_name : &'a str
+            struct_name : Cow<'a, str>
         }
 
         impl<'a> TypeTemplate<'a> for StructTemplate<'a> {}
 
         StructTemplate {
-            struct_name: ty.name.as_str()
+            struct_name: self.formatter.fmt_symbol_name(self.id.into())
         }
     }
 }
