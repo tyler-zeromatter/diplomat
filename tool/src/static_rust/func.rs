a55f5a7162535b938e99bea8db65adbe0bf644e6
Tyler from Zeromatter <tyler@0m.dev>
2025-08-20 10:03:49 -0700

    Start on return types

--- tool/src/static_rust/func.rs
+++ tool/src/static_rust/func.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 
 use askama::Template;
-use diplomat_core::hir::{MaybeOwn, Method, Mutability, SelfType};
+use diplomat_core::hir::{MaybeOwn, Method, Mutability, ReturnType, SelfType, SuccessType};
 
 use crate::static_rust::FileGenContext;
 
@@ -11,6 +11,7 @@ pub(super) struct FunctionInfo<'tcx> {
     name : Cow<'tcx, str>,
     abi_name : Cow<'tcx, str>,
     self_param : Option<MaybeOwn>,
+    return_type : Option<Cow<'tcx, str>>,
     params : Vec<ParamInfo<'tcx>>,
 }
 
@@ -35,7 +36,44 @@ impl<'tcx> FunctionInfo<'tcx> {
                 _ => unreachable!("Unknown SelfType: {:?}", s.ty)
             }
         });
-        FunctionInfo { name: method.name.as_str().into(), abi_name: method.abi_name.as_str().into(), params, self_param }
+
+        // TODO: DiplomatOption and DiplomatResult conversion support.
+        let return_type = match &method.output {
+            ReturnType::Fallible(ok, err) => {
+                let ok_ty = Self::gen_ok_type_name(&mut params, ctx, ok);
+                let err_ty = err.as_ref().map(|e| { ctx.gen_type_name(e) }).unwrap_or("()".into());
+                Some(format!("Result<{ok_ty}, {err_ty}>").into())
+            }
+            ReturnType::Nullable(ok) => {
+                let ok_ty = Self::gen_ok_type_name(&mut params, ctx, ok);
+                Some(format!("Option<{ok_ty}>").into())
+            }
+            ReturnType::Infallible(ok) => {
+                let type_name = Self::gen_ok_type_name(&mut params, ctx, ok);
+                if let SuccessType::OutType(o) = ok {
+                    Some(type_name)
+                } else {
+                    None
+                }
+            }
+        };
+
+        FunctionInfo { name: method.name.as_str().into(), abi_name: method.abi_name.as_str().into(), params, self_param, return_type }
+    }
+    
+    // TODO: &mut DiplomatWrite should only be true for the ABI version of params, not the return.
+    fn gen_ok_type_name(params : &mut Vec<ParamInfo>, ctx : &mut FileGenContext<'tcx>, ok : &'tcx SuccessType) -> Cow<'tcx, str>  {
+        match ok {
+            SuccessType::Unit => "()".into(),
+            SuccessType::OutType(o) => { 
+                ctx.gen_type_name(o)
+            }
+            SuccessType::Write => {
+                params.push(ParamInfo { var_name: "output".into(), type_name: "&mut DiplomatWrite".into() });
+                "()".into()
+            }
+            _ => panic!("HIR SuccessType {ok:?} unsupported")
+        }
     }
 
     pub(super) fn gen_function_block(ctx : &mut FileGenContext<'tcx>, functions : impl Iterator<Item = &'tcx Method>) -> Vec<FunctionInfo<'tcx>> {
