ebb1bba50860dc21e7b503313c6460d88a7655d7
Tyler from Zeromatter <tyler@0m.dev>
2025-08-20 15:16:58 -0700

    Test MyEnum

--- feature_tests/shared_rust/test/src/lib.rs
+++ feature_tests/shared_rust/test/src/lib.rs
@@ -3,15 +3,7 @@ mod tests {
     use shared_rust_feature_test_bindings::MyEnum;
     #[test]
     fn test_enum() {
-        let a = match MyEnum::get_a() {
-            MyEnum::A => "A",
-            MyEnum::B => "B",
-            MyEnum::C => "C",
-            MyEnum::D => "D",
-            MyEnum::F => "F",
-            MyEnum::E => "E"
-        };
-        assert_eq!(a, "A");
+        assert!(matches!(MyEnum::get_a(), MyEnum::A));
         let e = MyEnum::B;
         assert_eq!(e.into_value(), -1);
     }
