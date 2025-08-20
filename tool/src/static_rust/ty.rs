d41bb101d72e559b0b59ac7ab8347b46c9d34193
Tyler from Zeromatter <tyler@0m.dev>
2025-08-20 10:12:06 -0700

    Start on support for imports

--- tool/src/static_rust/ty.rs
+++ tool/src/static_rust/ty.rs
@@ -13,11 +13,13 @@ pub(super) struct FileGenContext<'tcx> {
     imports : BTreeSet<String>,
 }
 
-pub(super) trait TypeTemplate<'a> : Template {}
+pub(super) trait TypeTemplate<'a> : Template {
+    fn imports(&mut self) -> &mut BTreeSet<String>;
+}
 
 impl<'tcx, 'rcx> FileGenContext<'tcx> {
     pub(super) fn from_type<'a>(config : &Config, id : TypeId, formatter : &'a RustFormatter, tcx : &'a TypeContext) -> impl TypeTemplate<'a> {
-        let mut ctx = FileGenContext {
+        let ctx = FileGenContext {
             formatter,
             id: id.into(),
             tcx,
@@ -25,43 +27,63 @@ impl<'tcx, 'rcx> FileGenContext<'tcx> {
             imports: BTreeSet::new(),
         };
         let ty = ctx.tcx.resolve_type(id);
-        match ty {
+        let mut template = match ty {
             TypeDef::Struct(st) => {
                 ctx.generate_struct(st)
             }
             _ => panic!("Unsupported HIR type {ty:?}")
-        }
+        };
+        template.imports().remove::<str>(&formatter.fmt_symbol_name(id.into()));
+        template
     }
 
-    fn generate_struct(&'rcx mut self, ty : &'tcx StructDef) -> impl TypeTemplate<'tcx> {
+    fn generate_struct(mut self, ty : &'tcx StructDef) -> impl TypeTemplate<'tcx> {
         #[derive(Template)]
         #[template(path = "static_rust/base.rs.jinja", escape = "none")]
         struct StructTemplate<'a> {
             struct_name : Cow<'a, str>,
             methods : Vec<FunctionInfo<'a>>,
             lib_name: String,
+            imports : BTreeSet<String>,
         }
 
-        let methods = FunctionInfo::gen_function_block(self, ty.methods.iter());
+        let methods = FunctionInfo::gen_function_block(&mut self, ty.methods.iter());
 
-        impl<'a> TypeTemplate<'a> for StructTemplate<'a> {}
+        impl<'a> TypeTemplate<'a> for StructTemplate<'a> {
+            fn imports(&mut self) -> &mut BTreeSet<String> {
+                &mut self.imports
+            }
+        }
 
         StructTemplate {
             struct_name: self.formatter.fmt_symbol_name(self.id.into()),
             methods,
-            lib_name: self.lib_name.clone()
+            lib_name: self.lib_name,
+            imports: self.imports
         }
     }
 
     pub(super) fn gen_type_name<P: TyPosition>(&'rcx mut self, ty : &Type<P>) -> Cow<'tcx, str> {
         match ty {
+            Type::Primitive(p) => {
+                self.formatter.fmt_primitive_name(*p).into()
+            }
             Type::Struct(st) => {
                 let st_name = self.formatter.fmt_symbol_name(st.id().into());
                 self.imports.insert(st_name.clone().into());
                 st_name
             }
-            Type::Primitive(p) => {
-                self.formatter.fmt_primitive_name(*p).into()
+            Type::Enum(e) => {
+                let type_id : TypeId = e.tcx_id.into();
+                let enum_name = self.formatter.fmt_symbol_name(type_id.into());
+                self.imports.insert(enum_name.clone().into());
+                enum_name
+            }
+            Type::Opaque(op) => {
+                let type_id : TypeId = op.tcx_id.into();
+                let op_name = self.formatter.fmt_symbol_name(type_id.into());
+                self.imports.insert(op_name.clone().into());
+                op_name
             }
             _ => "TODO()".into()
         }
