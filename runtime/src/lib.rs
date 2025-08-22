8ba489579f28188301b792ce79b9de7f71c70412
Tyler from Zeromatter <tyler@0m.dev>
2025-08-22 11:42:28 -0700

    Expose buffer functions to Rust

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -10,7 +10,7 @@ mod wasm_glue;
 
 mod write;
 pub use write::DiplomatWrite;
-pub use write::{diplomat_buffer_write_create, diplomat_buffer_write_destroy};
+pub use write::{diplomat_buffer_write_create, diplomat_buffer_write_destroy, diplomat_buffer_write_get_bytes, diplomat_buffer_write_len};
 mod slices;
 pub use slices::{
     DiplomatOwnedSlice, DiplomatOwnedStr16Slice, DiplomatOwnedStrSlice, DiplomatOwnedUTF8StrSlice,
