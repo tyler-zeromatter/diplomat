e99066d8a2a058d9007a7b4a9fe543ee7af120c4
Tyler from Zeromatter <tyler@0m.dev>
2025-08-22 11:42:35 -0700

    Write new test

--- feature_tests/shared_rust/test/src/lib.rs
+++ feature_tests/shared_rust/test/src/lib.rs
@@ -1,10 +1,17 @@
 #[cfg(test)]
 mod tests {
-    use shared_rust_feature_test_bindings::MyEnum;
+    use shared_rust_feature_test_bindings::{MyEnum, OptionString};
     #[test]
     fn test_enum() {
         assert!(matches!(MyEnum::get_a(), MyEnum::A));
         let e = MyEnum::B;
         assert_eq!(e.into_value(), -1);
+    }
+
+    #[test]
+    fn test_string() {
+        let string = format!("This is a test");
+        let opt = OptionString::new(string.as_bytes()).unwrap();
+        assert_eq!(opt.write().unwrap(), string)
     }
 }
