35c91cd211e262a98b817c62b9632654fec2ce33
Tyler from Zeromatter <tyler@0m.dev>
2025-08-22 13:46:00 -0700

    Generation

--- feature_tests/shared_rust/bindings/src/my_string.rs
+++ feature_tests/shared_rust/bindings/src/my_string.rs
@@ -36,15 +36,12 @@ impl MyString {
             let len = diplomat_runtime::diplomat_buffer_write_len(write_ref);
     
             if !buf.is_null() {
+                // String takes ownership of the buffer:
                 String::from_raw_parts(buf, len, len)
             } else {
                 panic!("Could not read buffer, growth failed.")
             }
         };
-    
-        unsafe {
-            diplomat_runtime::diplomat_buffer_write_destroy(write);
-        }
         out_str
     }
 
@@ -63,15 +60,12 @@ impl MyString {
             let len = diplomat_runtime::diplomat_buffer_write_len(write_ref);
     
             if !buf.is_null() {
+                // String takes ownership of the buffer:
                 String::from_raw_parts(buf, len, len)
             } else {
                 panic!("Could not read buffer, growth failed.")
             }
         };
-    
-        unsafe {
-            diplomat_runtime::diplomat_buffer_write_destroy(write);
-        }
         out_str
     }
 
