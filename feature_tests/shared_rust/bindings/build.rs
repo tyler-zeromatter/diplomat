6ded2c1ac678a7fd015dcb3f1bbfdcae794fae76
Tyler from Zeromatter <tyler@0m.dev>
2025-08-22 14:53:06 -0700

    Start on build scripts

--- feature_tests/shared_rust/bindings/build.rs
+++ feature_tests/shared_rust/bindings/build.rs
@@ -1,10 +1,19 @@
+use std::env;
+
 fn main() {
-    println!("cargo::rustc-link-lib=dylib=somelib:diplomat_feature_tests.dll");
+    let out_dir = env::var("OUT_DIR").unwrap();
+    if cfg!(windows) {
+        println!("cargo::rustc-link-lib=dylib=somelib:diplomat_feature_tests.dll");
+        println!(r"cargo:rustc-link-search=dylib=../../target/debug");
+    } else {
+        println!(r"cargo:rustc-link-search={out_dir}");
+        println!("cargo::rustc-link-lib=somelib:diplomat_feature_tests");
+    }
+
+    if cfg!(windows) {
+        std::fs::copy("../../../target/debug/diplomat_feature_tests.dll", format!("../target/debug/diplomat_feature_tests.dll")).unwrap();
+    } else {
+        std::fs::copy("../../../target/debug/libdiplomat_feature_tests.so", format!("{out_dir}/libdiplomat_feature_tests.so")).unwrap();
+    }
     // We run `cargo test` from feature_tests/shared_rust, so we account for that path here:
-    println!(r"cargo:rustc-link-search=dylib=../../target/debug");
-    // if cfg!(windows) {
-    //     println!("cargo::rustc-link-lib=somelib:diplomat_feature_tests.dll");
-    // } else {
-    //     println!("cargo::rustc-link-lib=somelib:diplomat_feature_tests.dll");
-    // }
 }
