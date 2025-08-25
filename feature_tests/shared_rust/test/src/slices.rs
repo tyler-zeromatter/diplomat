804adcc3b7b774de92251ff5339b7dcebfb954bf
Tyler from Zeromatter <tyler@0m.dev>
2025-08-25 14:42:30 -0700

    Fixed valgrind issues

--- feature_tests/shared_rust/test/src/slices.rs
+++ feature_tests/shared_rust/test/src/slices.rs
@@ -5,7 +5,6 @@ mod tests {
     #[test]
     fn test_string() {
         let string = format!("This is a test");
-        // Run valgrind for this, it shows a memory leak:
         let opt = OptionString::new(string.as_bytes()).unwrap();
         assert_eq!(opt.write().unwrap(), string)
     }
