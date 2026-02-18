a3d29c9c39c0a12811118ffecb930dfef284498f
Tyler from Zeromatter <tyler@0m.dev>
2026-02-17 17:00:06 -0800

    Start on arc tests

--- feature_tests/src/opaque.rs
+++ feature_tests/src/opaque.rs
@@ -1,7 +1,9 @@
 #[diplomat::bridge]
 mod ffi {
+    use std::sync::Arc;
 
-    #[diplomat::opaque(wrapper=Box<T, Global>)]
+
+    #[diplomat::opaque(wrapper=Box<T>)]
     pub struct OpaqueAllocGlobal(String);
 
     impl OpaqueAllocGlobal {
@@ -10,11 +12,13 @@ mod ffi {
             Box::new(Self("Hello world".into()))
         }
     }
-
-    #[cfg(feature="allocator_trait")]
-    #[diplomat::cfg(feature=allocator_trait)]
-    mod custom_allocators {
-        #[diplomat::opaque(wrapper=Box<T, CustomAlloc>)]
-        pub struct CustomBoxAlloc;
+    
+    #[diplomat::opaque(wrapper=Arc<T>)]
+    pub struct OpaqueAllocArc(String);
+    impl OpaqueAllocArc {
+        #[diplomat::attr(auto, constructor)]
+        pub fn new() -> Arc<Self> {
+            
+        }
     }
 }
