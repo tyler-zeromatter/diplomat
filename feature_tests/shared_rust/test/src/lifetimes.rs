fb7fe1f47bc740a396bafb8cb9553b9d3e589d21
Tyler from Zeromatter <tyler@0m.dev>
2026-06-23 13:09:35 -0700

    Add lifetime bounds test

--- feature_tests/shared_rust/test/src/lifetimes.rs
+++ feature_tests/shared_rust/test/src/lifetimes.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use shared_rust_feature_test_bindings::{Foo};
+    use shared_rust_feature_test_bindings::{Foo, BorrowedFieldsWithBounds};
     #[test]
     fn test_lifetime_borrow() {
         let v = vec![0, 1, 2];
@@ -8,7 +8,17 @@ mod tests {
         // Uncomment to fail compilation, since `v` is borrowed for get_bar:
         // drop(v);
         let bar = f.get_bar();
-        assert_eq!(*foo.as_returning().bytes, [0, 1, 2]);
+        assert_eq!(*bar.foo().as_returning().bytes, [0, 1, 2]);
     }
-    // TODO: Test lifetime bounds.
+
+    #[test]
+    // If you remove the bounds on the lifetimes below, this will fail to compile:
+    fn test_bounds<'a, 'b : 'a, 'c: 'a + 'b>() {
+        let v = vec![0, 1, 2];
+        let foo = Foo::new(&v);
+        let dstr16 = vec![3, 4, 5];
+        let b = BorrowedFieldsWithBounds::<'a, 'b, 'c>::from_foo_and_strings(&foo, &dstr16, "utf8_str");
+        let st : &'c str = b.field_c.into();
+        assert_eq!(st, "utf8_str");
+    }
 }
