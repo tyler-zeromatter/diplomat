839529a24004fc2360ed59d91e0cebf8d50b63e7
Tyler from Zeromatter <tyler@0m.dev>
2026-09-04 14:33:24 -0700

    Python 3.8: Use `GetAttrString`

--- feature_tests/nanobind/src/sub_modules/somelib/FallibleTesterTrait_binding.cpp
+++ feature_tests/nanobind/src/sub_modules/somelib/FallibleTesterTrait_binding.cpp
@@ -79,7 +79,7 @@ void add_FallibleTesterTrait_binding(nb::module_ mod) {
     
         static inline void invalidMethod(PyObject* self, const char* method_name) {
             PyTypeObject* tp = Py_TYPE(self);
-            PyObject* tp_name = PyType_GetName(tp);
+            PyObject* tp_name = PyObject_GetAttrString((PyObject*)tp, "__name__");
             PyErr_Format(PyExc_AttributeError, "%U does not implement: '%s'", tp_name, method_name);
             Py_DECREF(tp_name);
         }
